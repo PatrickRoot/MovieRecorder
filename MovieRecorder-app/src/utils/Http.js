d4fbe1bc374b5116c5bd909b89658aec8a851f9c
Patrick <nianqinianyi@163.com>
2017-09-15 14:39:01 +0800

    网络请求 Util 类

--- MovieRecorder-app/src/utils/Http.js
+++ MovieRecorder-app/src/utils/Http.js
@@ -10,10 +10,126 @@
  * @time: 2017/9/14 21:45
  * @author: Patrick <root@sixlab.cn>
  */
-export function doPost(url, data, callback) {
+import * as ClientInfo from "../constants/clientInfo";
+import moment from "moment";
 
+export function doAuthPost(url, data, callback) {
+    doPost(url, data, callback, true)
 }
 
-export function doGet(url, callback) {
+export function doAuthGet(url, callback) {
+    doGet(url, callback, true)
+}
 
+export function doPost(url, data, callback, needAuth) {
+    let formData = transData(data);
+    
+    let options = {
+        method: 'POST',
+        body: formData,
+    };
+    doFetch(url, options, callback, needAuth);
+}
+
+export function doGet(url, callback, needAuth) {
+    let options = {
+        method: 'GET'
+    };
+    doFetch(url, options, callback, needAuth);
+}
+
+function doFetch(url, options, callback, needAuth) {
+    let accessToken = "";
+    if(needAuth){
+        accessToken = globalStore.getState().UserStore.accessToken;
+    }
+    
+    var fetchOptions = {
+        headers: {
+            'accessToken': accessToken,
+            'client': ClientInfo.clientOs,
+            'clientVersion': ClientInfo.clientVersion,
+            'version': ClientInfo.appVersion,
+            'Accept': 'application/json',
+            'Content-Type': 'multipart/form-data'
+        },
+        ...options
+    };
+    
+    fetch(url, fetchOptions)
+        .then((response) => response.text())
+        .then((responseText) => {
+            let data = {};
+            try {
+                data = JSON.parse(responseText);
+            } catch (e) {
+                data = {
+                    success: false,
+                    code: -2,
+                    message: "数据转换失败",
+                }
+            }
+            callback(data);
+        })
+        .catch(function (err) {
+            callback({
+                success: false,
+                code: -1,
+                message: err,
+            })
+        })
+        .done();
+}
+
+function transData(data) {
+    let formData = new FormData();
+    
+    if (data) {
+        objectParam(formData, data);
+    }
+    
+    return formData;
+}
+
+function objectParam(formData, data, superKey) {
+    for (let itemKey in data) {
+        let key = itemKey;
+        if (superKey) {
+            key = superKey + "." + itemKey;
+        }
+        let val = data[itemKey];
+        let type = typeof val;
+        switch (type) {
+            case "string":
+            case "number":
+                formData.append(key, val);
+                break;
+            case "boolean":
+                formData.append(key, val ? 1 : 0);
+                break;
+            case "boolean":
+                formData.append(key, val ? 1 : 0);
+                break;
+            case "object":
+                if (null !== val) {
+                    if (val instanceof Array) {
+                        arrayParam(formData, val, key);
+                    } else if (val instanceof Date) {
+                        formData.append(key, moment(val).format("YYYY-MM-DD HH:mm:ss"));
+                    } else if (val instanceof moment) {
+                        formData.append(key, val.format("YYYY-MM-DD HH:mm:ss"));
+                    } else {
+                        objectParam(formData, val, key);
+                    }
+                }
+                break;
+            
+        }
+    }
+}
+
+function arrayParam(formData, vals, key) {
+    for (let item of vals) {
+        formData.append(key, item);
+    }
 }

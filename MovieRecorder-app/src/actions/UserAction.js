efe1978e183040a5b32108cd2c2e8c1b52b6acb0
Patrick <nianqinianyi@163.com>
2017-09-15 10:39:53 +0800

    修改所有的注释

--- MovieRecorder-app/src/actions/UserAction.js
+++ MovieRecorder-app/src/actions/UserAction.js
@@ -1,10 +1,14 @@
 /**
- * Copyright (C) 2017 @sixlab, All Rights Reserved
- * Date: 2017/9/14
- * Time: 21:45
+ * Copyright (c) 2017 Sixlab. All rights reserved.
+ *
+ * Under the GPLv3(AKA GNU GENERAL PUBLIC LICENSE Version 3).
+ * see http://www.gnu.org/licenses/gpl-3.0-standalone.html
+ *
+ * For more information, please see
+ * https://sixlab.cn/
+ *
+ * @time: 2017/9/14 21:45
  * @author: Patrick <root@sixlab.cn>
- * @link: https://code.sixlab.cn/
- * @licence: GPLv3
  */
 import {
     Toast,
